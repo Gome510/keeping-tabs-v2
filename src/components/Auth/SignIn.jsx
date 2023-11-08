f62207db0a9473ec08299947cb70768bd4f193fe
Gome510 <bluehawkfire55@gmail.com>
2023-11-08 12:01:01 -0800

    sign in done, styling sidebar

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -14,11 +14,16 @@ function SignIn({ setIsAuth }) {
   }
 
   return (
-    <div className="d-flex justify-content-center text-center align-items-center h-full">
-      <div className="sign-in rounded ">
-        <h1>Sign In</h1>
-        <button className="rounded border-0" onClick={signInWithGoogle}>
-          Sign In With Google
+    <div className="sign-in-container ">
+      <div className="sign-in ">
+        <div>
+          <h1 className="brand">Keeping Tabs</h1>
+          <h6>Sign In To Chat</h6>
+        </div>
+
+        <button className="button " onClick={signInWithGoogle}>
+          <img width={30} height={30} src="/assets/images/search.png" />
+          {"  "}Sign In With Google
         </button>
       </div>
     </div>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Sidebar.css";
 
 function Sidebar() {
-  return <div className="">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-content">sidebar content</div>
+      <div className="sidebar-content">sidebar content</div>
+    </div>
+  );
 }
 
 export default Sidebar;
+
+function SidebarItem() {
+  return <div>Sidebar</div>;
+}
