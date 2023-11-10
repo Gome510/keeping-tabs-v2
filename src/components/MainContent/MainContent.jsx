efff648aa9440ba4860a4a6c8f9b50f8dc6b048e
Gome510 <bluehawkfire55@gmail.com>
2023-11-10 01:21:33 +0000

    unselected room message

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import "./MainContent.css";
 import { db } from "../../firebase/firebase-config";
 
-function MainContent() {
-  return <div className="main-content">MainContent</div>;
+function MainContent({currentRoom}) {
+  if (currentRoom == "") {
+     return <div className="main-content"> Select a chat room</div>;
+  }
+
+  //load messages
+  
+  
 }
 
 export default MainContent;
