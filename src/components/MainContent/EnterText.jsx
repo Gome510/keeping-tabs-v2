47deaef1a719ab57a94f40bfe3e8f39902ddb1e4
Gome510 <bluehawkfire55@gmail.com>
2023-11-11 22:34:41 -0800

    input text area

--- src/components/MainContent/EnterText.jsx
+++ src/components/MainContent/EnterText.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { db, auth } from "../../firebase/firebase-config";
 
-function EnterText() {
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+
+function EnterText({ currentRoom }) {
+  const [newMessage, setNewMessage] = useState("");
+  const messageRef = collection(db, "messages");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (newMessage == "") {
+      return;
+    }
+
+    try {
+      await addDoc(messageRef, {
+        text: newMessage,
+        room: currentRoom,
+        createdAt: serverTimestamp(),
+        user: auth.currentUser.displayName,
+        userId: auth.currentUser.uid,
+        pfp: auth.currentUser.photoURL,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    setNewMessage("");
+  }
+
   return (
-    <div className="enter-text">
-      <textarea className="enter-text-box" placeholder="Say something nice!" />
-      <button className="send-button">Send</button>
-    </div>
+    <form className="enter-text" onSubmit={handleSubmit}>
+      <textarea
+        onChange={(e) => setNewMessage(e.target.value)}
+        className="enter-text-box"
+        placeholder="Say something nice!"
+        value={newMessage}
+      />
+      <button type="submit" className="send-button">
+        Send
+      </button>
+    </form>
   );
 }
 
