f39975177601a89936109d20d237e1239501f65e
Gome510 <bluehawkfire55@gmail.com>
2023-11-14 23:12:11 -0800

    modal ui

--- src/components/Sidebar/AddRoomModal.jsx
+++ src/components/Sidebar/AddRoomModal.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddRoomModal.css";
 
 function AddRoomModal({ setOpenModal }) {
+  //TODO: "Room exists already" and "Room does not exist" warnings
+  const [room, setRoom] = useState("");
+
+  function handleJoin() {}
+
+  function handleCreate() {}
+
   return (
     <div className="room-modal-backdrop">
       <div className="room-modal">
         <button id="exit-modal-button" onClick={() => setOpenModal(false)}>
           X
         </button>
-        <button className="room-modal-header">Create a Room</button>
-        <button className="room-modal-header">Join a Room</button>
+        <h1>Create or Join a Room</h1>
+        <input
+          placeholder="Enter Room Name"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        />
+
+        <div className="d-flex justify-content-around ">
+          <button className="room-modal-option" onClick={handleCreate}>
+            Create
+          </button>
+          <button className="room-modal-option" onClick={handleJoin}>
+            Join
+          </button>
+        </div>
       </div>
     </div>
   );
