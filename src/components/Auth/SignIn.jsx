975398ff93a51e4f564a0eac848343454206ea91
Pablo Gomez-Echegaray <pablogomezechegaray@gmail.com>
2025-03-16 15:38:16 -0700

    Fix background on signin

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,48 +1,69 @@
-import React from "react";
-import { auth, provider, db } from "../../firebase/firebase-config";
-import { signInWithPopup, getAdditionalUserInfo } from "firebase/auth";
-import { collection, addDoc } from "firebase/firestore";
-import Cookies from "universal-cookie";
+import { useEffect, useRef, useState } from "react";
+
+import NET from 'vanta/dist/vanta.net.min'
+
 import "./SignIn.css";
+import { signInWithGoogle } from "../../firebase/auth";
 
-const cookie = new Cookies();
-const userRef = collection(db, "users");
 
-function SignIn({ setIsAuth }) {
-  async function signInWithGoogle() {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      cookie.set("auth-token", result.user.refreshToken);
+function SignIn() {
+  const [isSigningIn, setIsSigningIn] = useState(false);
 
-      //add new user document to database
-      const { isNewUser } = getAdditionalUserInfo(result);
-      if (isNewUser) {
-        console.log("New user");
-        await addDoc(userRef, {
-          user: auth.currentUser.displayName,
-          userId: auth.currentUser.uid,
-          rooms: [],
-        });
-      } else {
-        console.log("Returning user");
-        // Handle returning user here
+
+  const [vantaEffect, setVantaEffect] = useState(null);
+  const bgRef = useRef(null)
+
+  useEffect(()=>{
+    if(!vantaEffect){
+      setVantaEffect(
+        NET({
+          el: bgRef.current,
+          mouseControls: false,
+          touchControls: false,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          color: 0xc53e6d,
+          backgroundColor: 0x0,
+          points: 12.00,
+          maxDistance: 26.00,
+          spacing: 20.00
+        })
+      );
+    }
+
+    return () => {
+      if(vantaEffect) vantaEffect.destroy();
+    }
+  }, [vantaEffect])
+
+  async function handleButtonClick(e) {
+    e.preventDefault()
+
+    if(!isSigningIn){
+      setIsSigningIn(true);
+      try {
+        await signInWithGoogle();
+      } catch (error) {
+        console.error(error);
       }
-
-      setIsAuth(true);
-    } catch (error) {
-      console.log(error);
+      setIsSigningIn(false)
     }
   }
 
+
+
   return (
-    <div className="sign-in-container ">
+    <div className="sign-in-container " ref={bgRef}>
       <div className="sign-in ">
         <div>
           <h1 className="brand">Keeping Tabs</h1>
           <h6>Sign In To Chat</h6>
         </div>
 
-        <button className="button " onClick={signInWithGoogle}>
+        <button className="button " onClick={handleButtonClick} disabled={isSigningIn}>
           <img width={30} height={30} src="/assets/images/search.png" />
           {"  "}Sign In With Google
         </button>
